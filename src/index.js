c75a92b76e4aa8fd42360fcd3cb7e6f7c99df886
mohsin <mohsinbwp2018@gmail.com>
2022-08-01 14:18:00 +0500

    final

---
+++ src/index.js
@@ -0,0 +1,21 @@
+ 
+
+  let i=0;
+
+  function move(i)
+  {
+    if(i<1)
+    {
+      document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/55299e51264213136f7d6a43_hoop.jpg)";
+    }
+    if(i>3)
+    {
+      document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/55299d1b0108cf394df61d5a_sheep.jpeg)";
+  }
+if(i=2)
+{
+  document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/5527a395e02754bb57746389_slider1.jpg)";
+  
+}
+}
+
